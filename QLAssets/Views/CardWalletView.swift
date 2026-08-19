c5597360ed078ce70fff89db6d00693b02957b78
乔磊 <547160185@qq.com>
2026-08-19 16:12:09 +0800

    Refactor code structure for improved readability and maintainability

--- QLAssets/Views/CardWalletView.swift
+++ QLAssets/Views/CardWalletView.swift
@@ -5182,7 +5182,7 @@ struct EditCardView: View {
                 )
                 .frame(
                     width: 105,
-                    minHeight: 34
+                    height: 34
                 )
 
                 Picker(
